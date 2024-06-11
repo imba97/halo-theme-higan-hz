dc4b5fb75690914efa57a6f23bfe86284ed97c8c
HowieHz <howie_hzgo@outlook.com>
2024-06-11 09:11:24 +0800

    chore: 调整构建临时文件位置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import "unfonts.css";
-import "./assets/colors/theme.css";
 import "./assets/main.js";
-import "./styles/main.css";
-import "./styles/tailwind-css.css";
+import "../tmp/styles/main.css";
+import "../tmp/styles/tailwind.css";
+import "../tmp/styles/theme.css";
 import "@purge-icons/generated";
 import "github-syntax-light/lib/github-light.css";
 export * from "./create-toc";
